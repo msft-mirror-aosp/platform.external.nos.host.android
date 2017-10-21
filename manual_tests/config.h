cb61e62ff269ebb44b0de3d298b87595415a8f70
Bill Richardson <wfrichar@google.com>
2017-10-20 23:20:34 -0700

    cleanup: manual_tests/config.h is no longer needed
    Change-Id: Iba387b6e9cdc69f69e60cea86d822c8af774a0bc
    Signed-off-by: Bill Richardson <wfrichar@google.com>

--- manual_tests/config.h
+++
@@ -1,8 +0,0 @@
-/*
- * This is just here until I can fix the headers, which are in another
- * git repo. Pay no attention to that man behind the curtain.
- */
-#ifndef __STUPID_CONFIG_H
-#define __STUPID_CONFIG_H
-#define CHIP_FLASH_BANK_SIZE 2048
-#endif /* __STUPID_CONFIG_H */
