fbb3011a5dbab73aed73536e024cb68726b8d2a8
Bill Richardson <wfrichar@google.com>
2019-02-07 10:32:01 -0800

    citadeld: request and cache low-power stats
    This requests the low-power stats from Citadel, following each
    burst of AP-to-Citadel traffic. The stats are merely cached for
    now. A future CLs will make use of them.
    
    Bug: 123950560
    Test: manual
    
      Watched Citadel console to observe request appear (once), only
      after 0.5 seconds have elapsed since the last SPI transaction.
    
    Change-Id: Ifd72a6ce9724c0db901356c3ec01254b54cec9c6
    Signed-off-by: Bill Richardson <wfrichar@google.com>

--- citadel/citadeld/main.cpp
+++ citadel/citadeld/main.cpp
@@ -14,16 +14,21 @@
  * limitations under the License.
  */
 
+#include <algorithm>
+#include <chrono>
+#include <condition_variable>
+#include <functional>
 #include <limits>
-#include <thread>
 #include <mutex>
+#include <thread>
 
 #include <android-base/logging.h>
 #include <binder/IPCThreadState.h>
 #include <binder/IServiceManager.h>
 
-#include <nos/device.h>
+#include <app_nugget.h>
 #include <nos/NuggetClient.h>
+#include <nos/device.h>
 
 #include <android/hardware/citadel/BnCitadeld.h>
 
@@ -44,30 +49,86 @@ using ::android::hardware::citadel::ICitadeld;
 
 namespace {
 
+using namespace std::chrono_literals;
+
+// This attaches a timer to a function call. Call .schedule() to start the
+// timer, and the function will be called (once) after the time has elapsed. If
+// you call .schedule() again before that happens, it just restarts the timer.
+// There's no way to cancel the function call after it's scheduled; you can only
+// postpone it.
+class DeferredCallback {
+  public:
+    DeferredCallback(std::chrono::milliseconds delay, std::function<void()> fn)
+        : _armed(false),
+          _delay(delay),
+          _func(fn),
+          _waiter_thread(std::bind(&DeferredCallback::waiter_task, this)) {}
+    ~DeferredCallback() {}
+
+    // [re]start the timer for the delayed call
+    void schedule() {
+        std::unique_lock<std::mutex> _lock(_cv_mutex);
+        _armed = true;
+        _cv.notify_one();
+    }
+
+  private:
+    void waiter_task(void) {
+        std::unique_lock<std::mutex> _lock(_cv_mutex);
+        while (true) {
+            if (!_armed) {
+                _cv.wait(_lock);
+            }
+            auto timeout = std::chrono::steady_clock::now() + _delay;
+            if (_cv.wait_until(_lock, timeout) == std::cv_status::timeout) {
+                _func();
+                _armed = false;
+            }
+        }
+    }
+
+    bool _armed;
+    const std::chrono::milliseconds _delay;
+    const std::function<void()> _func;
+    std::thread _waiter_thread;
+    std::mutex _cv_mutex;
+    std::condition_variable _cv;
+};
+
 class CitadelProxy : public BnCitadeld {
-public:
-    CitadelProxy(NuggetClient& client) : _client{client} {}
+  public:
+    CitadelProxy(NuggetClient& client)
+        : _client{client},
+          _stats_collection(500ms, std::bind(&CitadelProxy::cacheStats, this)) {
+    }
     ~CitadelProxy() override = default;
 
-    Status callApp(const int32_t _appId, const int32_t _arg, const std::vector<uint8_t>& request,
-                   std::vector<uint8_t>* const response, int32_t* const _aidl_return) override {
-        // AIDL doesn't support integers less than 32-bit so validate it before casting
+    Status callApp(const int32_t _appId, const int32_t _arg,
+                   const std::vector<uint8_t>& request,
+                   std::vector<uint8_t>* const response,
+                   int32_t* const _aidl_return) override {
+        // AIDL doesn't support integers less than 32-bit so validate it before
+        // casting
         if (_appId < 0 || _appId > kMaxAppId) {
             LOG(ERROR) << "App ID " << _appId << " is outside the app ID range";
             return Status::fromExceptionCode(Status::EX_ILLEGAL_ARGUMENT);
         }
         if (_arg < 0 || _arg > std::numeric_limits<uint16_t>::max()) {
-            LOG(ERROR) << "Argument " << _arg << " is outside the unsigned 16-bit range";
+            LOG(ERROR) << "Argument " << _arg
+                       << " is outside the unsigned 16-bit range";
             return Status::fromExceptionCode(Status::EX_ILLEGAL_ARGUMENT);
         }
 
         const uint8_t appId = static_cast<uint32_t>(_appId);
-        const uint16_t arg =  static_cast<uint16_t>(_arg);
+        const uint16_t arg = static_cast<uint16_t>(_arg);
         uint32_t* const appStatus = reinterpret_cast<uint32_t*>(_aidl_return);
 
         // Make the call to the app while holding the lock for that app
         std::unique_lock<std::mutex> lock(_appLocks[appId]);
         *appStatus = _client.CallApp(appId, arg, request, response);
+
+        _stats_collection.schedule();
+
         return Status::ok();
     }
 
@@ -84,6 +145,24 @@ private:
 
     NuggetClient& _client;
     std::mutex _appLocks[kMaxAppId + 1];
+    struct nugget_app_low_power_stats _stats;
+    DeferredCallback _stats_collection;
+
+    void cacheStats(void) {
+        std::vector<uint8_t> buffer;
+        buffer.reserve(sizeof(_stats));
+        uint32_t rv;
+
+        // Make the call to the app while holding the lock for that app
+        std::unique_lock<std::mutex> lock(_appLocks[APP_ID_NUGGET]);
+        rv = _client.CallApp(APP_ID_NUGGET, NUGGET_PARAM_GET_LOW_POWER_STATS,
+                             buffer, &buffer);
+
+        if (rv == APP_SUCCESS) {
+            memcpy(&_stats, buffer.data(),
+                   std::min(sizeof(_stats), buffer.size()));
+        }
+    }
 };
 
 [[noreturn]] void CitadelEventDispatcher(const nos_device& device) {
